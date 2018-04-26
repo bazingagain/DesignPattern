90e6a0d6831dee6b0d7e141448d52cf6250812a1
bazingagain <1152204424@qq.com>
2018-04-26 22:14:20 +0800

    add bridge facade composite

---
+++ BridgePattern/src/main/java/com/leon/bridgepattern/MyBridge.java
@@ -0,0 +1,9 @@
+package com.leon.bridgepattern;
+
+/**
+ * Created on 2018/4/26.
+ *
+ * @author Xiaolei-Peng
+ */
+public class MyBridge {
+}
---
+++ CompositePattern/src/main/java/com/leon/compositepattern/TreeNode.java
@@ -0,0 +1,9 @@
+package com.leon.compositepattern;
+
+/**
+ * Created on 2018/4/26.
+ *
+ * @author Xiaolei-Peng
+ */
+public class TreeNode {
+}
